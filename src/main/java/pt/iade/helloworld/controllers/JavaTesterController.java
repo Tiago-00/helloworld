25e6e0cc17887d9ff796f95a9857179e83f63f02
Tiago Vitor Nunes da Silva <tiaguinhoo956@gmail.com>
2020-11-06 16:47:12 +0000

    update

--- src/main/java/pt/iade/helloworld/controllers/JavaTesterController.java
+++ src/main/java/pt/iade/helloworld/controllers/JavaTesterController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,18 +27,49 @@ public class JavaTesterController {
 
 
     @PostMapping(path = "/units", produces= MediaType.APPLICATION_JSON_VALUE)
-    public CurricularUnit saveUnit(@RequestBody CurricularUnit unit) {
-    logger.info("Added unit "+unit.getName());
-    units.add(unit);
-    return unit;
+        public CurricularUnit saveUnit(@RequestBody CurricularUnit unit) {
+        logger.info("Added unit "+unit.getName());
+            units.add(unit);
+                return unit;
  }
 
     @GetMapping(path = "/units",
-    produces= MediaType.APPLICATION_JSON_VALUE)
-    public ArrayList<CurricularUnit> getUnits() {
-        logger.info("Get "+units.size()+" Units");
-        return units;
+        produces= MediaType.APPLICATION_JSON_VALUE)
+        public ArrayList<CurricularUnit> getUnits() {
+            logger.info("Get "+units.size()+" Units");
+                return units;
     } 
 
+    @GetMapping(path = "/access/{student}/{covid}", 
+        produces= MediaType.APPLICATION_JSON_VALUE)
+        public boolean getGreeting(@PathVariable("student") boolean isStudent,
+            @PathVariable("covid") boolean hasCovid) {
+                return isStudent && !hasCovid;
+    
+ }
+
+    @GetMapping(path = "/required/{student}/{temperature}/{classType}",
+        produces= MediaType.APPLICATION_JSON_VALUE)
+        public boolean getRequired(@PathVariable("student") boolean isStudent,
+            @PathVariable("temperature") double hasCovid,
+            @PathVariable("classType") String type) {
+                return isStudent && type.equals("presential") && (hasCovid > 34.5 && hasCovid <37.5 ) ;
+     }
+
+
+     @GetMapping(path = "/evacuation/{fire}/{numberOfCovids}/{powerShutdown}/{comeBackTime}/",
+        produces= MediaType.APPLICATION_JSON_VALUE)
+        public boolean getRequired(@PathVariable("fire") boolean isfire,
+            @PathVariable("numberOfCovids") int numberOfCovids,
+            @PathVariable("powerShutdown") boolean powerShutdown,
+            @PathVariable("comeBackTime") int comeBackTime) {
+                return isfire && (numberOfCovids >5 || powerShutdown) && comeBackTime >15;
+
+    }
 
 }
+
+
+
+
+
